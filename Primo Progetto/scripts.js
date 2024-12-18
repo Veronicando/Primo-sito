6ca74d525baf6c3ffade8418e411ba84d6d3dbde
Veronicando <vfgd2024@gmail.com>
2024-12-18 16:19:21 +0000

    pseudopasticcio

---
+++ Primo Progetto/scripts.js
@@ -0,0 +1,6 @@
+function mostramessaggio() {
+    document.getElementById("message").textContent = "Grazie per aver intrapreso questo percorso";
+}
+   
+         document.getElementById("bf").addEventListener("click", mostramessaggio);
+
