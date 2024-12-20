ec1500b98952067c14be76303283ba4c64e97516
Veronicando <vfgd2024@gmail.com>
2024-12-20 18:17:32 +0000

    new version

--- Primo Progetto/scripts.js
+++ Primo Progetto/scripts.js
@@ -1,14 +1,16 @@
 function mostramessaggio() {
-					
-    var fm = document.getElementById("femmina").value;
-              var ms =	document.getElementById("maschio").value;
-              var sesso = fm, ms;
-      var message;
-              
-    if (sesso = fm) {
-           message = "Grazie per aver intrapreso questo percorso";
-        } else if (sesso = ms) {
-            message = "Siamo spiacenti, questo percorso è riservato al genere femminile. Per perseguire la tua vocazione ti consigliamo di visitare il seguente link";
-       }
-      document.getElementById("message").innerText = message;
- }
+  var fm = document.getElementById("femmina").value
+  var ms = document.getElementById("maschio").value
+  var sessoval = document.getElementById("sesso").value
+  var link = document.getElementById("myLink").href
+  let message
+
+  if (sessoval == fm) {
+    message = "Grazie per aver intrapreso questo percorso"
+  } else if (sessoval == ms) {
+    message =
+      "Siamo spiacenti, questo percorso è riservato al genere femminile. Per perseguire la tua vocazione ti consigliamo di visitare il seguente link" +
+      link
+  }
+  document.getElementById("message").innerText = message
+}
