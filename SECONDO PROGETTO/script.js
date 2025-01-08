3ee5e20098c8e96da69b9c608637100e5bc9384c
Veronicando <vfgd2024@gmail.com>
2025-01-08 18:13:05 +0000

    agg2.0

---
+++ SECONDO PROGETTO/script.js
@@ -0,0 +1,16 @@
+function mostramessaggio(event) {
+  var fm = document.getElementById("femmina").value
+  var ms = document.getElementById("maschio").value
+  var sessoval = document.getElementById("sesso").value
+  var link = document.getElementById("myLink").href
+  let message
+
+  if (sessoval === fm) {
+    message = "Grazie per aver intrapreso questo percorso"
+  } else if (sessoval === ms) {
+    message =
+      "Siamo spiacenti, questo percorso è riservato al genere femminile. Per perseguire la tua vocazione ti consigliamo di visitare il seguente link " + "<a href= '" +
+      link + "'>Clicca qui</a>"
+  }
+  document.getElementById("message").innerHTML = message
+}
