c4eee66c494d08b972a688ef2daed708b0ccb2bb
Veronicando <vfgd2024@gmail.com>
2025-01-27 18:45:14 +0000

    db

--- PHP/login.php
+++ PHP/login.php
@@ -70,3 +70,77 @@ if(isset($_POST["Login"])){
         </form>
     </body>
 </html>
+
+// chat version
+<form action="login.php" method="POST">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <input type="submit" value="Login">
+</form>
+
+<?php
+// Dati dell'utente (array di esempio)
+$persona1 = array("marco@email.it", "12345", "Marco", "Rossi");
+$persona2 = array("luigi@email.it", "abcdef", "Luigi", "Bianchi");
+$persona3 = array("luca@email.it", "123abc", "Luca", "Verdi");
+
+// Verifica se l'utente è già loggato tramite cookie
+if (isset($_COOKIE['user_email']) && isset($_COOKIE['user_name'])) {
+    echo "Bentornato " . $_COOKIE['user_name'];
+    echo "<br><a href='logout.php'>Disconnetti</a>";
+    exit();
+}
+
+// Verifica se sono stati inviati i dati dal form
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Controlliamo che email e password non siano vuoti
+    if (empty($email) || empty($password)) {
+        echo "Utente o password non validi.";
+        exit();
+    }
+
+    // Verifica delle credenziali
+    $found = false;
+    $name = "";
+    $surname = "";
+
+    // Confrontiamo i dati inviati con i dati delle persone registrate
+    $persone = [$persona1, $persona2, $persona3];
+    foreach ($persone as $persona) {
+        if ($persona[0] === $email && $persona[1] === $password) {
+            $found = true;
+            $name = $persona[2];
+            $surname = $persona[3];
+            break;
+        }
+    }
+
+    // Se l'utente è trovato, creiamo il cookie e mostriamo il messaggio
+    if ($found) {
+        echo "Bentornato " . $name . " " . $surname;
+        setcookie('user_email', $email, time() + 3600); // Il cookie dura 1 ora
+        setcookie('user_name', $name . " " . $surname, time() + 3600);
+        echo "<br><a href='logout.php'>Disconnetti</a>";
+    } else {
+        echo "Utente o password non validi.";
+    }
+}
+?>
+
+<?php
+// Rimuoviamo i cookie
+setcookie('user_email', '', time() - 3600);
+setcookie('user_name', '', time() - 3600);
+
+// Reindirizziamo alla pagina di login
+header("Location: login_form.html");
+exit();
+?>
+
